3ebf59bb360a033c32895ca018021de777f21753
Hamzah Marie <mariehamzah1@gmail.com>
2025-07-07 15:03:29 -0400

    Add messagebox validation and confirmation to password saving

--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -1,9 +1,33 @@
+from tkinter import *
+from tkinter import messagebox
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+
+    website = web_input.get()
+    email = email_input.get()
+    password = pw_input.get()
+
+    if len(website) == 0 and len(password) == 0:
+        msg = messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
+    else:
+
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\nPassword: {password}\nIs it okay to save?")
+
+        if is_ok:
+            with open ("data.txt", mode="a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+                web_input.delete(0, END)
+                pw_input.delete(0, END)
+
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
-from tkinter import *
+
 
 #Create Window
 window = Tk()
@@ -29,9 +53,11 @@ pw_label.grid(row=3, column=0)
 #Entries
 web_input = Entry(width=35)
 web_input.grid(row=1, column=1, columnspan=2)
+web_input.focus()
 
 email_input = Entry(width=35)
 email_input.grid(row=2, column= 1, columnspan=2)
+email_input.insert(0, "test@gmail.com")
 
 pw_input = Entry(width=21)
 pw_input.grid(row=3, column=1)
@@ -40,7 +66,7 @@ pw_input.grid(row=3, column=1)
 generate_pw_btn = Button(text="Generate Password")
 generate_pw_btn.grid(row=3, column=2)
 
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=save_password)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 
