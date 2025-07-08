7100b7ee446cfbe8c980f16adf657095a2b97473
Hamzah Marie <mariehamzah1@gmail.com>
2025-07-08 02:24:31 -0400

    Refactor code, use built in pandas method to convert data frame to organize dataframe into a records list of dictionaries

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -6,29 +6,30 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
 
-#-------------------------GENERATE A RANDOM WORD TO DISPLAY-----------------------------
-def random_word():
-    key,value = random.choice(list(new_dict.items()))
-    canvas.itemconfig(word, text=key)
-
-
-
-
-
+# Manually constructed dict from columns (just for fun)
+# new_dict = {data_dict["French"][i] : data_dict["English"][i] for i in range(len(data_dict["French"]))}
+# print(new_dict)
 
 #Read data from french_words.csv
 data = pandas.read_csv("data/french_words.csv")
-data_dict = data.to_dict()
-print(data_dict)
+to_learn = data.to_dict(orient="records")
 
-# Manually constructed dict from columns (just for fun)
-new_dict = {data_dict["French"][i] : data_dict["English"][i] for i in range(len(data_dict["French"]))}
-print(new_dict)
+
+
+#-------------------------GENERATE A RANDOM WORD TO DISPLAY-----------------------------
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
+
+
 
 
 
 #Create window
 window = Tk()
+window.title("Learn French")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 #create card image to put on canvas representing the front of the card
@@ -39,19 +40,19 @@ canvas.config(highlightthickness=0, bg=BACKGROUND_COLOR)
 canvas.grid(row=0, column=0, columnspan=2)
 
 #Text on canvas
-title = canvas.create_text(400, 150, text="French", font=(FONT_NAME,40, "italic"))
-word = canvas.create_text(400, 263, text="word", font=(FONT_NAME,60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=(FONT_NAME,40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=(FONT_NAME,60, "bold"))
 
 #Buttons
 incorrect_img = PhotoImage(file="images/wrong.png")
-incorrect_btn = Button(image=incorrect_img, highlightthickness=0, command=random_word)
+incorrect_btn = Button(image=incorrect_img, highlightthickness=0, command=next_card)
 incorrect_btn.grid(row=1, column=0)
 
 correct_img = PhotoImage(file="images/right.png")
-correct_btn = Button(image=correct_img, highlightthickness=0, command=random_word)
+correct_btn = Button(image=correct_img, highlightthickness=0, command=next_card)
 correct_btn.grid(row=1, column=1)
 
 
-
+next_card()
 
 window.mainloop()
