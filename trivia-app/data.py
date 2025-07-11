cabd121eda5b410095fa9bc396dabe8fdc60c58e
Hamzah Marie <mariehamzah1@gmail.com>
2025-07-11 09:48:53 -0400

    Change type of questions to be only true/false. Used html module in quiz_brain to ensure the questions are unenscaped from HTML entities.

--- trivia-app/data.py
+++ trivia-app/data.py
@@ -1,7 +1,8 @@
 import requests
 API_URL = "https://opentdb.com/api.php?amount=10"
 parameters = {
-    "amount": 10
+    "amount": 10,
+    "type": "boolean"
 }
 
 data = requests.get(url=API_URL, params=parameters).json()["results"]
